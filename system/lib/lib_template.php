a4cfb8bf477c249ded00042748eb8b1a73919fd3
yuwen41200 <yuwen41200@gmail.com>
2014-11-18 02:21:13 +0800

    Rewrited lib/lib_template

--- system/lib/lib_template.php
+++ system/lib/lib_template.php
@@ -1,63 +1,31 @@
 <?php
 
-/**
- * 模板类
- * @copyright   Copyright(c) 2011
- * @author      yuansir <yuansir@live.cn/yuansir-web.com>
- * @version     1.0
- */
 final class Template {
-        public $template_name = null;
-        public $data = array();
-        public $out_put = null;
-        
-        public function init($template_name,$data = array()) {
-                $this->template_name = $template_name;
-                $this->data = $data;
-                $this->fetch();
-        }
-        /**
-         * 加载模板文件
-         * @access      public
-         * @param       string  $file
-         */
-        public function fetch() {
-                $view_file = _VIEW_PATH . '/' . $this->template_name . '.php';
-                if (file_exists($view_file)) {
-                        extract($this->data);
-                        ob_start();
-                        include $view_file;
-                        $content = ob_get_contents();
-                        ob_end_clean();
-                        $this->out_put =  $content;
-                } else {
-                        trigger_error('加载 ' . $view_file . ' 模板不存在');
-                }
-        }
-        /**
-         * 输出模板
-         * @access      public  
-         * @return      string
-         */
-        public function output(){
-                echo $this->out_put;
-        }
-        /**
-         * 写入静态化文件
-         * @access      public  
-         */
-//        public function toHtml() {
-//                if (!is_dir(ROOT_PATH . '/cache/template/')) {
-//                        mkdir(ROOT_PATH . '/cache/template/', 0777);
-//                }
-//                if (!$fp = @fopen(ROOT_PATH . '/cache/template/' . $filename . '.html', 'w')) {
-//                        trigger_error('文件 ' . ROOT_PATH . '/cache/template/' . $filename . '.html' . ' 不能打开');
-//                }
-//                if (fwrite($fp, $content) == FALSE) {
-//                        trigger_error('文件 ' . ROOT_PATH . '/cache/template/' . $filename . '.html' . ' 写入失败');
-//                }
-//                fclose($fp);
-//        }
+	public $view = NULL;
+	public $data = array();
+	public $result = NULL;
+	
+	public function init($view, $data) {
+		$this -> view = $view;
+		$this -> data = $data;
+		$this -> result = $this -> fetchContents();
+	}
 
+	public function fetchContents() {
+		$view_file = _VIEW_PATH.'/'.$this -> view.'.php';
+		if (file_exists($view_file)) {
+			extract($this -> data);
+			ob_start();
+			require $view_file;
+			return ob_get_clean();
+		}
+		else
+			return NULL;
+	}
+
+	public function output() {
+		echo $this -> result;
+	}
 }
 
+?>
